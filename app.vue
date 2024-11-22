c9555de7f4ea155609516dbf6aaaf7e5850d70cd
Pjotr Surkov <pjotr@live.com>
2024-11-22 14:32:41 +0200

    feat: create Vue page for displaying cryptocurrency prices

--- app.vue
+++ app.vue
@@ -1,6 +1,39 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <SelectButton
+    v-model="pair"
+    :options="[
+      PairEnum.TonUsdt,
+      PairEnum.UsdtTon,
+    ]"
+  />
+  <DataTable :value="[price]">
+    <Column
+      field="pair"
+      header="Pair"
+    />
+    <Column
+      field="rate"
+      header="Rate"
+    />
+    <Column
+      field="site"
+      header="Site"
+    />
+    <Column
+      field="time"
+      header="Time"
+    />
+  </DataTable>
 </template>
+
+<script setup lang="ts">
+import { PairEnum, type PriceType } from '~/types/price'
+
+const pair = ref<PairEnum>(PairEnum.TonUsdt)
+const price = ref<PriceType | null>(null)
+
+watchEffect(async () => {
+  const { data } = await useFetch<PriceType>(`/api/prices/${pair.value}`)
+  price.value = data.value
+})
+</script>
